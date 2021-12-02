5f1139bd58ebea3ff2314482fdde9de580cc888d
ApacheTech Solutions <p_matthews@live.co.uk>
2021-12-02 21:49:46 +0000

    YAEMC - Yet Another Extension Methods Commit
     - Added Tavis JSON Patcher Extensions Methods for API 1.16.
     - Minor readability changes to Class Registry Extensions.

--- ApacheTech.VintageMods.Core/Extensions/Game/ClassRegistryExtensions.cs
+++ ApacheTech.VintageMods.Core/Extensions/Game/ClassRegistryExtensions.cs
@@ -4,7 +4,7 @@
 // ReSharper disable UnusedMember.Global
 
 namespace ApacheTech.VintageMods.Core.Extensions.Game
-{
+{ 
     public static class ClassRegistryExtensions
     {
         /// <summary>
@@ -12,12 +12,12 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Item to register.</typeparam>
+        /// <typeparam name="T">The type of the Item to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Item to add to the registry.</param>
-        public static void RegisterItem<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterItem<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterItemClass(name, typeof(TItemClass));
+            api.RegisterItemClass(name, typeof(T));
         }
 
         /// <summary>
@@ -25,12 +25,12 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Block to register.</typeparam>
+        /// <typeparam name="T">The type of the Block to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Block to add to the registry.</param>
-        public static void RegisterBlock<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterBlock<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterBlockClass(name, typeof(TItemClass));
+            api.RegisterBlockClass(name, typeof(T));
         }
 
         /// <summary>
@@ -38,12 +38,12 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Block to register.</typeparam>
+        /// <typeparam name="T">The type of the Block to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Block to add to the registry.</param>
-        public static void RegisterBlockEntity<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterBlockEntity<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterBlockEntityClass(name, typeof(TItemClass));
+            api.RegisterBlockEntityClass(name, typeof(T));
         }
 
         /// <summary>
@@ -51,12 +51,12 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Block Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the Block Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Block Behaviour to add to the registry.</param>
-        public static void RegisterBlockBehaviour<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterBlockBehaviour<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterBlockBehaviorClass(name, typeof(TItemClass));
+            api.RegisterBlockBehaviorClass(name, typeof(T));
         }
 
         /// <summary>
@@ -64,12 +64,12 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the BlockEntity Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the BlockEntity Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the BlockEntity Behaviour to add to the registry.</param>
-        public static void RegisterBlockEntityBehaviour<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterBlockEntityBehaviour<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterBlockEntityBehaviorClass(name, typeof(TItemClass));
+            api.RegisterBlockEntityBehaviorClass(name, typeof(T));
         }
 
         /// <summary>
@@ -77,48 +77,48 @@ namespace ApacheTech.VintageMods.Core.Extensions.Game
         ///     Must happen before any blocks are loaded. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Block Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the Block Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Block Behaviour to add to the registry.</param>
-        public static void RegisterEntity<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterEntity<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterEntity(name, typeof(TItemClass));
+            api.RegisterEntity(name, typeof(T));
         }
 
         /// <summary>
         ///     Registers a new Entity Behaviour. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Entity Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the Entity Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Entity Behaviour to add to the registry.</param>
-        public static void RegisterEntityBehaviour<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterEntityBehaviour<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterEntityBehaviorClass(name, typeof(TItemClass));
+            api.RegisterEntityBehaviorClass(name, typeof(T));
         }
 
         /// <summary>
         ///     Registers a new Crop Behaviour. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Crop Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the Crop Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Crop Behaviour to add to the registry.</param>
-        public static void RegisterCropBehaviour<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterCropBehaviour<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterCropBehavior(name, typeof(TItemClass));
+            api.RegisterCropBehavior(name, typeof(T));
         }
 
         /// <summary>
         ///     Registers a new Collectible Behaviour. <br/>
         ///     Must be registered on both the client, and server.
         /// </summary>
-        /// <typeparam name="TItemClass">The type of the Collectible Behaviour to register.</typeparam>
+        /// <typeparam name="T">The type of the Collectible Behaviour to register.</typeparam>
         /// <param name="api">The game's internal API.</param>
         /// <param name="name">The name of the Collectible Behaviour to add to the registry.</param>
-        public static void RegisterCollectibleBehaviour<TItemClass>(this ICoreAPICommon api, string name = nameof(TItemClass))
+        public static void RegisterCollectibleBehaviour<T>(this ICoreAPICommon api, string name = nameof(T))
         {
-            api.RegisterCollectibleBehaviorClass(name, typeof(TItemClass));
+            api.RegisterCollectibleBehaviorClass(name, typeof(T));
         }
     }
 }
---
+++ ApacheTech.VintageMods.Core/Extensions/Game/TavisExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Vintagestory.ServerMods.NoObf;
+
+namespace ApacheTech.VintageMods.Core.Extensions.Game
+{
+    /// <summary>
+    ///     Extension methods for Tavis JSON Patching Engine.
+    /// </summary>
+    public static class TavisExtensions
+    {
+        /// <summary>
+        ///     Applies a single patch to a JSON file.
+        /// </summary>
+        /// <param name="jsonPatcher">The <see cref="ModJsonPatchLoader"/> ModSystem used to patch JSON files in the game.</param>
+        /// <param name="patch">The patch to apply.</param>
+        public static void ApplyPatch(this ModJsonPatchLoader jsonPatcher, JsonPatch patch)
+        {
+            jsonPatcher.ApplyPatches(new List<JsonPatch>{patch});
+        }
+
+        /// <summary>
+        ///     Applies a number of patches to the JSON assets of the game.
+        /// </summary>
+        /// <param name="jsonPatcher">The <see cref="ModJsonPatchLoader"/> ModSystem used to patch JSON files in the game.</param>
+        /// <param name="patches">The patches to apply.</param>
+        public static void ApplyPatches(this ModJsonPatchLoader jsonPatcher, List<JsonPatch> patches)
+        {
+            for (var i = 0; i < patches.Count; i++)
+            {
+                var applied = 0;
+                var notFound = 0;
+                var errorCount = 0;
+                var patch = patches[i];
+                jsonPatcher.ApplyPatch(i, patch.File, patch, ref applied, ref notFound, ref errorCount);
+            }
+        }
+    }
+}
